987d3583b5e475893437c24ab7f50f9206891f2a
sitarameez <sitarablossom@gmail.com>
2021-06-25 20:20:21 -0400

    NumbersTriangles lab completed

--- TableUtilities.java
+++ TableUtilities.java
@@ -4,51 +4,52 @@ import java.util.StringJoiner;
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
         int size = 5;
-        //StringBuilder a = new StringBuilder();
-        StringJoiner a = new StringJoiner("  ");
+        int multipliedValue = 0;
+        String multipliedValueInString = "";
+        String numberOfSpaces = "";
+        StringBuilder a = new StringBuilder();
         for(int i=1;i<=size;i++){
             
             for(int j=1;j<=size;j++){
-               // a.append(i);
-            //a.append("|");
-                /*if(j==1)
-                {
-                    a.append("");
+                multipliedValue = i*j;
+                multipliedValueInString = String.valueOf(multipliedValue);
+                numberOfSpaces = "  ";
+                if(multipliedValueInString.length() == 2){
+                    numberOfSpaces = " ";
                 }
-                else
-                {
-                    a.append(" ");
-                }*/
-                //int i2 = i*j;
-                a.add(String.valueOf(i*j));
-                //a.append(" ");
-                //a.append("|");
-                a.add("|");
+                else if(multipliedValueInString.length() == 3){
+                    numberOfSpaces = "";
+                }
+                a.append(numberOfSpaces);
+                a.append(multipliedValue);      
+                a.append(" |");
             }
-            a.add(System.lineSeparator());
+            a.append(System.lineSeparator());
         }
         return a.toString();
     }
 
     public static String getLargeMultiplicationTable() {
-         int size = 10;
+          int size = 10;
+        int multipliedValue = 0;
+        String multipliedValueInString = "";
+        String numberOfSpaces = "";
         StringBuilder a = new StringBuilder();
         for(int i=1;i<=size;i++){
             
             for(int j=1;j<=size;j++){
-               // a.append(i);
-            //a.append("|");
-                if(j==1)
-                {
-                    a.append("");
+                multipliedValue = i*j;
+                multipliedValueInString = String.valueOf(multipliedValue);
+                numberOfSpaces = "  ";
+                if(multipliedValueInString.length() == 2){
+                    numberOfSpaces = " ";
                 }
-                else
-                {
-                    a.append(" ");
+                else if(multipliedValueInString.length() == 3){
+                    numberOfSpaces = "";
                 }
-                a.append(i*j);
-                a.append(" ");
-                a.append("|");
+                a.append(numberOfSpaces);
+                a.append(multipliedValue);      
+                a.append(" |");
             }
             a.append(System.lineSeparator());
         }
@@ -56,24 +57,26 @@ public class TableUtilities {
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        int size = tableSize;
+         int size = tableSize;
+        int multipliedValue = 0;
+        String multipliedValueInString = "";
+        String numberOfSpaces = "";
         StringBuilder a = new StringBuilder();
         for(int i=1;i<=size;i++){
             
             for(int j=1;j<=size;j++){
-               // a.append(i);
-            //a.append("|");
-                if(j==1)
-                {
-                    a.append("");
+                multipliedValue = i*j;
+                multipliedValueInString = String.valueOf(multipliedValue);
+                numberOfSpaces = "  ";
+                if(multipliedValueInString.length() == 2){
+                    numberOfSpaces = " ";
                 }
-                else
-                {
-                    a.append(" ");
+                else if(multipliedValueInString.length() == 3){
+                    numberOfSpaces = "";
                 }
-                a.append(i*j);
-                a.append(" ");
-                a.append("|");
+                a.append(numberOfSpaces);
+                a.append(multipliedValue);      
+                a.append(" |");
             }
             a.append(System.lineSeparator());
         }
